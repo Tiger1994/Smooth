f63e11143806c5af75bcd3ff32cae091503ce3a4
server <1300288499@qq.com>
2020-04-12 16:01:54 +0800

    test OK!

--- model/residual.py
+++ model/residual.py
@@ -53,10 +53,10 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.conv_input = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv_input = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
         self.relu = nn.LeakyReLU(0.2, inplace=True)
 
-        self.convt_R = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.convt_R = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3, stride=1, padding=1, bias=False)
         self.convt_F = self.make_layer(_Conv_Block)
 
         for m in self.modules():
